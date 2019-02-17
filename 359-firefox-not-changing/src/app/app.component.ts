962a3fa74c2efd46128c170e68756ced250aa3cf
Tjoskar <mr.karlsson.oskar@gmail.com>
2019-02-17 16:27:05 +0100

    ✨

--- 359-firefox-not-changing/src/app/app.component.ts
+++ 359-firefox-not-changing/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  screen_lg = '500px';
+  defaultImage = 'https://www.placecage.com/1000/1000';
+  image1 = 'https://images.unsplash.com/photo-1443890923422-7819ed4101c0?fm=jpg';
+  image2 = 'https://images.unsplash.com/photo-1550246520-56dedb9b4a1e?ixlib=rb-1.2.1&auto=format&fit=crop&w=3401&q=80'
+  image3 = 'https://images.unsplash.com/photo-1550308576-cd091bbfd97b?ixlib=rb-1.2.1&auto=format&fit=crop&w=3300&q=80'
 }
--- 359-firefox-not-changing/src/app/app.module.ts
+++ 359-firefox-not-changing/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LazyLoadImageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
