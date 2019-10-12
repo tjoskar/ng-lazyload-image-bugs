99444c4cac6d6150292ec21feb0b7907be809cc1
tjoskar <kontakta@oskarkarlsson.nu>
2019-10-12 11:25:23 +0200

    add 408

---
+++ 408-svg-issue/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html"
+})
+export class AppComponent {
+  defaultImage = "https://www.placecage.com/1000/1000";
+  image = "https://s.cdpn.io/3/kiwi.svg";
+}
---
+++ 408-svg-issue/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {
+  LazyLoadImageModule,
+  intersectionObserverPreset
+} from "ng-lazyload-image";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    LazyLoadImageModule.forRoot({
+      preset: intersectionObserverPreset
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
