1bf60bcbf6133751c41f52cd3d837614dfb60bd0
tjoskar <kontakta@oskarkarlsson.nu>
2020-06-05 10:03:29 +0200

    🚀

--- 463-invalid-provider-for-lazyLoadImageHook-libs/projects/my-lib/src/lib/my-lib.component.ts
+++ 463-invalid-provider-for-lazyLoadImageHook-libs/projects/my-lib/src/lib/my-lib.component.ts
@@ -2,15 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-my-lib',
-  template: `
-    <p>
-      my-lib works!
-    </p>
-  `,
+  template: ` <img [defaultImage]="defaultImage" [lazyLoad]="image" /> `,
   styles: [
   ]
 })
 export class MyLibComponent implements OnInit {
+  defaultImage = 'https://www.placecage.com/1000/1000';
+  image = 'https://images.unsplash.com/photo-1443890923422-7819ed4101c0?fm=jpg';
 
   constructor() { }
 
--- 463-invalid-provider-for-lazyLoadImageHook-libs/projects/my-lib/src/lib/my-lib.module.ts
+++ 463-invalid-provider-for-lazyLoadImageHook-libs/projects/my-lib/src/lib/my-lib.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MyLibComponent } from './my-lib.component';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 
 
 @NgModule({
   declarations: [MyLibComponent],
   imports: [
+    LazyLoadImageModule
   ],
   exports: [MyLibComponent]
 })
