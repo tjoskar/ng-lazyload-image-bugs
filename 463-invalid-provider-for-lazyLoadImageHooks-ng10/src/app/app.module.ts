d65e575df1b0aa5e707ba3b0a3de61219bf5b708
tjoskar <kontakta@oskarkarlsson.nu>
2020-08-23 16:45:36 +0200

    update

--- 463-invalid-provider-for-lazyLoadImageHooks-ng10/src/app/app.module.ts
+++ 463-invalid-provider-for-lazyLoadImageHooks-ng10/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { NgModule, Injectable } from '@angular/core';
+import { LazyLoadImageModule, IntersectionObserverHooks, Attributes, LAZYLOAD_IMAGE_HOOKS } from 'ng-lazyload-image';
 
 import { AppComponent } from './app.component';
+
+@Injectable()
+export class LazyLoadImageHooks extends IntersectionObserverHooks {
+  async loadImage({ imagePath }: Attributes): Promise<string> {
+    console.log(imagePath);
+    return await fetch(imagePath, {
+      headers: {
+        Authorization: 'Bearer ...',
+      },
+    })
+      .then((res) => res.blob())
+      .then((blob) => URL.createObjectURL(blob));
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // imports: [BrowserModule, LazyLoadImageModule.forRoot(LazyLoadImageHooks)],
   imports: [BrowserModule, LazyLoadImageModule],
-  providers: [],
+  providers: [{ provide: LAZYLOAD_IMAGE_HOOKS, useClass: LazyLoadImageHooks }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
