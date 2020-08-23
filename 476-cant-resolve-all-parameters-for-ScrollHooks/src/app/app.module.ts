778e0dad851a61dc8375e4d7788ff5ab9916188f
tjoskar <kontakta@oskarkarlsson.nu>
2020-08-23 17:10:06 +0200

    add ng lazy load

--- 476-cant-resolve-all-parameters-for-ScrollHooks/src/app/app.module.ts
+++ 476-cant-resolve-all-parameters-for-ScrollHooks/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LazyLoadImageModule, LAZYLOAD_IMAGE_HOOKS, ScrollHooks } from 'ng-lazyload-image';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, LazyLoadImageModule],
+  providers: [{ provide: LAZYLOAD_IMAGE_HOOKS, useClass: ScrollHooks }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
