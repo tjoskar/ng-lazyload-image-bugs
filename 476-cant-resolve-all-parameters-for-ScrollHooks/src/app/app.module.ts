0dd886045c76e431b7aa725acefa8fb78c9d445f
tjoskar <kontakta@oskarkarlsson.nu>
2020-08-23 17:40:21 +0200

    fix

--- 476-cant-resolve-all-parameters-for-ScrollHooks/src/app/app.module.ts
+++ 476-cant-resolve-all-parameters-for-ScrollHooks/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { LazyLoadImageModule, LAZYLOAD_IMAGE_HOOKS, ScrollHooks } from 'ng-lazyload-image';
 import { AppComponent } from './app.component';
 
+export class MyScrollHooks extends ScrollHooks {
+  constructor() {
+    super()
+  }
+}
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, LazyLoadImageModule],
-  providers: [{ provide: LAZYLOAD_IMAGE_HOOKS, useClass: ScrollHooks }],
+  providers: [{ provide: LAZYLOAD_IMAGE_HOOKS, useClass: MyScrollHooks }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
